df7b2d7f99311c210bf42752156a7b8f9cf080e9
Nevto <Jonathannorberg95@gmail.com>
2024-05-07 18:12:17 +0200

    added basic logging functionality

--- middleware/logger.mjs
+++ middleware/logger.mjs
@@ -1,13 +1,17 @@
 import fs from 'fs';
 import path from 'path';
+import { URL } from 'url';
+
+
+// const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const logMyData = (req, res, next) => {
-    const logFilePath = path.join(__dirname, '../logs', 'unity.log')
-    console.log('Logging data to: ', logFilePath);
+    const logFilePath = path.join(__appdir, 'logs', 'unity.log')
+
 
     const message = `${req.method}  ${req.originalUrl} ${new Date()}`;
 
-    false.appendFileSync(logFilePath, message);
+    fs.appendFileSync(logFilePath, message);
 
     next()
 }
--- server.mjs
+++ server.mjs
@@ -1,9 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import unityRouter from './routes/unityRouter.mjs'
+import logMyData from './middleware/logger.mjs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 dotenv.config({ path: './config/config.env' })
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(logMyData)
+}
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+console.log(dirname);
+global.__appdir = dirname
+
 
 const PORT = process.env.PORT || 5000
 
